20116449136078d0caff0fe26cdb8d053528c30b
ZaidKhan <zaidkhan5213@gmail.com>
2023-02-01 13:31:02 -0500

    endgame time lock

--- src/Endgame.cpp
+++ src/Endgame.cpp
@@ -6,26 +6,39 @@
 
 void Endgame_Fire(void *)
 {
+  double startTime, driveTime, deployTime;
+  bool wasDisabled = true;
+  if(wasDisabled){
+    startTime = pros::millis();
+  }
+  //set how long the driver run will be
+  //change this number
+  driveTime = 60;
+  //do math for time
+  deployTime = driveTime*1000 - 10000;
   while (true)
   {
-    int presses = 0;
-    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT) && presses == 1)
-    {
-      Endgame.set_value(true);
-    }
-    else {
-      Endgame.set_value(false);
-      SageWall.set_value(false);
-    }
+    if(pros::millis() - startTime == deployTime){
 
-    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT))
-    {
-      SageWall.set_value(true);
-      pros::delay(250);
-      presses++;
-    }
+      int presses = 0;
+      if (master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT) && presses == 1)
+      {
+        Endgame.set_value(true);
+      }
+      else {
+        Endgame.set_value(false);
+        SageWall.set_value(false);
+      }
 
-    pros::delay(20);
+      if (master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT))
+      {
+        SageWall.set_value(true);
+        pros::delay(250);
+        presses++;
+      }
+
+      pros::delay(20);
+    }
   }
 }
 
